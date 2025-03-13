e725a2b4c1c68dce0460d8632c1b3b47d3010c2d
Gabriel vidal <devzerounivassouras@gmail.com>
2025-03-13 19:58:57 -0300

    aula 13

--- projeto/.venv/Scripts/activate.bat
+++ projeto/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Gabriel Vidal\backend\backend\projeto\.venv"
+set "VIRTUAL_ENV=C:\Users\UNIVASSOURAS\backend\backend\projeto\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
